7824e03f4aa889f415c7566fc23e81e60133fc19
Manoel-Diger <armenteiromanoel@gmail.com>
2025-07-21 20:50:26 -0300

    Aplicadas melhorias na bateria virtual

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.body.addEventListener('keyup', (event)=>{
-    playSound(event.code.toLowerCase() );
+    playSound( event.code.toLowerCase() );
 });
 
 document.querySelector('.composer button').addEventListener('click', () => {
@@ -11,9 +11,9 @@ document.querySelector('.composer button').addEventListener('click', () => {
     }
 });
 
-function playSound(sound){
+function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
-    let keyElement = document.querySelector(`div[data-key=${sound}]`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
     if(audioElement) {
         audioElement.currentTime = 0;
@@ -23,7 +23,7 @@ function playSound(sound){
     if(keyElement) {
         keyElement.classList.add('active');
 
-        setTimeout(() =>{
+        setTimeout(()=>{
             keyElement.classList.remove('active');
         }, 300);
     }
@@ -38,6 +38,5 @@ function playComposition(songArray) {
         }, wait);
 
         wait += 250;
-        
     }
 }
